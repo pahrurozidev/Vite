4e35ec40c164d9ec63dba1f161177b60a2cdb911
Pahrurozi <61068212+pahrurozidev@users.noreply.github.com>
2022-12-05 18:21:25 +0800

    dfdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import logo from './logo.svg'
 import './App.css'
 
@@ -7,12 +7,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={'Route /'} />
         <Route path='/dashboard' element={'Route /dashboard'} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
